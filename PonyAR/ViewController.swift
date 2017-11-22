b1bca69840d3b1701f8f4d217fc0ccbaa2c12a1f
Rohit Navalgund <legorohit111@gmail.com>
2017-11-22 09:57:39 +0530

    Initial Commit
    This is an initial Commit.

---
+++ PonyAR/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  PonyAR
+//
+//  Created by Rohit Navalgund on 19/11/17.
+//  Copyright © 2017 RNavalgund. All rights reserved.
+//
+//  This PonyAR is made by Rohit Navalgund (@rnavalgund_) and Daniel (@danield3v). We hold the rights to sue you for
+//  Stealing our code. Especially Leon freaking shit.
+//  (I am sure you are not aware of ARKit or anything related to coding though Leon)
+//
+
+import UIKit
+import SceneKit
+import SceneKit.ModelIO
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        let scene = SCNScene.init(named: "art.scnassets/pony.scn")!
+        sceneView.scene = scene
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
